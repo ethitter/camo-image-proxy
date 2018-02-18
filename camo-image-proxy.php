e4da09ad7110f13f67bbf9b3494f37131889c797
Erick Hitter <git-contrib@ethitter.com>
2018-02-18 09:08:26 -0800

    Scaffold start

---
+++ camo-image-proxy.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Camo Image Proxy
+ * Plugin URI:      https://ethitter.com/plugins/
+ * Description:     Rewrite image URLs to use a Camo image proxy.
+ * Author:          Erick Hitter
+ * Author URI:      https://ethitter.com/
+ * Text Domain:     camo-image-proxy
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Camo_Image_Proxy
+ */
+
+// Your code starts here.
